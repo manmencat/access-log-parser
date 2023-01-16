889dd6f87d1efe9f2d873846a3951d50c5e3e57e
Maksim Petrov <mapetrov@inno.tech>
2023-01-16 18:37:05 +0300

    Добавлен подсчет минимальной, максимальной длин строк, добавлен подсчет количества строк

--- Workers/src/main/java/ru/courses/workers/Dep.java
+++
@@ -1,6 +0,0 @@
-package ru.courses.workers;
-
-public class Dep {
-    public String name;
-    public Worker chief;
-}
--- packages/src/main/java/ru/courses/main/Main.java
+++
@@ -1,6 +0,0 @@
-package ru.courses.main;
-
-public class Main {
-    public static void main(String[] args) {
-    }
-}
