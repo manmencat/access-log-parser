1227452ab27d1ff0eb1961910830ed344229b6af
Maksim Petrov <mapetrov@inno.tech>
2023-01-17 19:37:07 +0300

    Добавлен вывод в консоль количества строк YandexBot и GoogleBot

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         String path;
-        int count =0;
+        int count = 0;
         while (true) {
             System.out.println("Введите путь к файлу и нажмите <Enter>:");
             path = new Scanner(System.in).nextLine();
@@ -22,8 +22,8 @@ public class Main {
 
                     String line;
                     int linesCount = 0;
-                    int minLineLength = Integer.MAX_VALUE;
-                    int maxLineLength = 0;
+                    int googleBotCount = 0;
+                    int yandexBotCount = 0;
                     while ((line = reader.readLine()) != null) {
                         int length = line.length();
 
@@ -31,27 +31,52 @@ public class Main {
                             throw new RuntimeException("Length of line > 1024");
                         }
 
-                        if (length > maxLineLength) {
-                            maxLineLength = length;
+                        char bracket = '"';
+                        int[] bracketIndices = new int[6];
+                        bracketIndices[0] = line.indexOf(bracket);
+                        bracketIndices[1] = line.indexOf(bracket, bracketIndices[0] + 1);
+                        bracketIndices[2] = line.indexOf(bracket, bracketIndices[1] + 1);
+                        bracketIndices[3] = line.indexOf(bracket, bracketIndices[2] + 1);
+                        bracketIndices[4] = line.indexOf(bracket, bracketIndices[3] + 1);
+                        bracketIndices[5] = line.indexOf(bracket, bracketIndices[4] + 1);
+
+                        String thirdBrackets = line.substring(bracketIndices[4], bracketIndices[5]);
+
+                        String[] parts = thirdBrackets.split(";");
+                        for (int i = 0; i < parts.length; i++) {
+                            parts[i] = parts[i].trim();
                         }
-                        if (length < minLineLength) {
-                            minLineLength = length;
+                        if (parts.length >= 2) {
+                            String fragment = parts[1];
+
+                            int slashIndex = fragment.indexOf('/');
+                            if (slashIndex > 0) {
+                                String bot = fragment.substring(0, slashIndex);
+                                if (bot.equalsIgnoreCase("Googlebot")) {
+                                    googleBotCount++;
+                                } else if (bot.equalsIgnoreCase("YandexBot")) {
+                                    yandexBotCount++;
+                                }
+                            }
                         }
+
                         linesCount++;
                     }
 
-                    System.out.println("Count of line: " + linesCount);
-                    System.out.println("Max line length: " + maxLineLength);
-                    System.out.println("Min line length: " + minLineLength);
+                    System.out.println("Lines count: " + linesCount);
+                    System.out.println("GoogleBot count: " + googleBotCount + " (" + ((double)googleBotCount / (double)linesCount * 100.0) + "%)");
+                    System.out.println("YandexBot count: " + yandexBotCount + " (" + (double)yandexBotCount / (double)linesCount * 100.0 + "%)");
                 } catch (FileNotFoundException e) {
                     System.out.println(e.fillInStackTrace());
                 } catch (IOException e) {
                     System.out.println(e.fillInStackTrace());
-                };
+
             }
-            System.out.println("Файл отсутствует или путь ведет к директории файла");
+
 
         }
+            else {
+                System.out.println("Файл отсутствует или путь ведет к директории файла");
+            }
     }
-}
-
+}}
