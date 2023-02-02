b11f53c226612e0bf90e142e2f59bb1bd669ac97
Maksim Petrov <mapetrov@inno.tech>
2023-02-02 18:34:22 +0300

    Добавлен расчет доли браузеров

--- src/Statistics.java
+++ src/Statistics.java
@@ -9,7 +9,9 @@ public class Statistics {
     LogEntry[] entries;
     int lastEntryIndex = 0;
     final HashSet<String> okPages;
+    final HashSet<String> notFoundPages;
     final HashMap<String, Integer> osStat;
+    final HashMap<String, Integer> browserStat;
     long totalTraffic = 0;
     LocalDateTime minTime = LocalDateTime.MAX;
     LocalDateTime maxTime = LocalDateTime.MIN;
@@ -17,7 +19,9 @@ public class Statistics {
     Statistics() {
         entries = new LogEntry[200000];
         okPages = new HashSet<>();
+        notFoundPages = new HashSet<>();
         osStat = new HashMap<>();
+        browserStat = new HashMap<>();
     }
 
     public void addEntry(LogEntry entry) {
@@ -35,28 +39,41 @@ public class Statistics {
 
         if (entry.answerCode.equals("200")) {
             okPages.add(entry.path);
+        } else if (entry.answerCode.equals("404")) {
+            notFoundPages.add(entry.path);
         }
 
         String os = entry.userAgent.operatingSystem;
         osStat.put(os, osStat.getOrDefault(os, 0) + 1);
+
+        String browser = entry.userAgent.browser;
+        browserStat.put(browser, browserStat.getOrDefault(browser, 0) + 1);
     }
 
     public int getTrafficRate() {
         long hours = ChronoUnit.HOURS.between(minTime, maxTime);
         if (hours == 0)
             hours = 1;
-        return (int)(totalTraffic / hours);
+        return (int) (totalTraffic / hours);
     }
 
     public List<String> getOkPages() {
         return Arrays.asList(okPages.toArray(new String[0]));
     }
 
-    public HashMap<String, Double> getPlatformStatistics() {
+    private static HashMap<String, Double> getStatistics(HashMap<String, Integer> statMap) {
         HashMap<String, Double> result = new HashMap<>();
-        for (var entry : osStat.entrySet()) {
-            result.put(entry.getKey(), Double.valueOf(entry.getValue()) / osStat.size());
+        for (var entry : statMap.entrySet()) {
+            result.put(entry.getKey(), Double.valueOf(entry.getValue()) / statMap.size());
         }
         return result;
+    }
+
+    public HashMap<String, Double> getPlatformStatistics() {
+        return getStatistics(osStat);
+    }
+
+    public HashMap<String, Double> getBrowserStatistics() {
+        return getStatistics(browserStat);
     }
 }
